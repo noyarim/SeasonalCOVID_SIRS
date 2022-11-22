42cb80add71a8b0baa3cf120fd4ff395339dc40a
Kyueun Lee <noyarim@gmail.com>
2022-11-22 09:47:11 -0800

    update fit_beta.R
    estimate monthly vs biweekly beta
    
    save ggplot

--- 01_fit_beta.R
+++ 01_fit_beta.R
@@ -24,7 +24,7 @@ ode_SIRS <- function(t, state, parameters){
     if(t ==0){
       w_idx = 1
     }else{
-      w_idx = floor(t/30.5)
+      w_idx = floor(t/n_days_per_unit)
       }
     # find week index
     m.beta.d <- m.beta[w_idx]
@@ -63,24 +63,28 @@ run_SIRS <- function(parameters){
     #   IC = c(S=1-1000/187000000,I=1000/187000000,R=0,C=0)
     # }
     # Initial distribution
-    IC = c(S=1-0.0000015, I=0.0000015, R=0, C=0)
+    IC = c(S=1-0.000015, I=0.000015, R=0, C=0)
     # run ODE solver
     odetime <- proc.time()
-    #prob = rk(y=IC, times=tspan, func = ode_SIRS, parms = parameters, method='rk45dp7')
-    prob = rk(y = IC, times = tspan, func = ode_SIRS, parms = parameters, method='rk45dp7',atol = 10^-8, rtol = 10^-10)  # TODO: constrain domain for x to be 0 or positive (x>0)
+    prob = rk(y=IC, times=tspan, func = ode_SIRS, parms = parameters, method='rk45dp7')
+    #prob = rk(y = IC, times = tspan, func = ode_SIRS, parms = parameters, method='rk45dp7',atol = 10^-8, rtol = 10^-10)  # TODO: constrain domain for x to be 0 or positive (x>0)
     proc.time() - odetime
     return(prob)
   })
 } 
 
 ## A function to calculate the new cases ##
-cal_case <- function(prob){
+cal_case <- function(prob, scale){
   new_case <-(prob[-1,5]- prob[-nrow(prob),5])*10^5
   new_case[1] = 0
-  new_case_log <- log(new_case)
-  new_case_log[is.infinite(new_case_log)] = 0
-  #print(new_case[1:10])
-  return(new_case_log)
+  if (scale %in% c('log','Log')){
+    new_case_log <- log(new_case)
+    new_case_log[is.infinite(new_case_log)] = 0
+    return(new_case_log)
+  }else{
+    return(new_case)
+  }
+
 }
 
 ## A function to calculate error ##
@@ -89,7 +93,7 @@ cal_cost <- function(m.beta, parameters, data, grouping, group){
     parameters$m.beta = m.beta
     # model outcome
     prob <- run_SIRS(parameters)
-    I.m <- cal_case(prob)
+    I.m <- cal_case(prob, 'log')
     # observed case 
     temp <- data %>%
       filter(!!sym(grouping) == group) %>%
@@ -120,7 +124,7 @@ cal_cost <- function(m.beta, parameters, data, grouping, group){
 run_SIRS_clbr <- function(m.beta, parameters){
   parameters$m.beta = m.beta
   prob = run_SIRS(parameters)
-  I.m <- cal_case(prob)
+  I.m <- cal_case(prob,'natural')
   
   return(list(prob,I.m))
 }
@@ -154,31 +158,33 @@ covid_by_state2 <- covid_by_state %>%
 T0 = 0.0
 Tend = length(levels(as.factor(covid_by_state2$date)))
 n_days = as.integer(Tend-T0)
-
+n_days_per_unit = 30.5
 # Fixed infection parameters
 beta0=0.185
 gamma= 0.16
 z = 0.005
 
 # varying parameters (weekly beta multiplier)
-m.beta = rep(1,as.integer(n_days/30.5))
+m.beta = rep(1,as.integer(n_days/n_days_per_unit))
 
 # parameters to run SIRS model
 parameters = list(
                   T0 = T0,
                   Tend = Tend,
                   n_days = n_days,
+                  n_days_per_unit = n_days_per_unit,
                   InfectionParam=c(beta0,gamma,z)
                   )
 
 # Directed search of weekly beta values by group  
-grp1_NE<-optim(m.beta, fn = cal_cost, parameters = parameters, data=covid_by_state2, grouping = 'grp1', group='NE', method="Nelder-Mead")
-grp1_NE<-optim(m.beta, fn = cal_cost, parameters = parameters, data=covid_by_state2, grouping = 'grp1', group='NE', lower=rep(0.5,length(m.beta)), upper=rep(1.5,length(m.beta)), method="L-BFGS-B")
 grp1_NE<-optim(m.beta, fn = cal_cost, parameters = parameters, data=covid_by_state2, grouping = 'grp1', group='NE', method="BFGS")
-
-grp1_S<-optim(m.beta, fn = cal_cost, parameters = parameters, data=covid_by_state2, grouping = 'grp1', group='S')
-grp1_W<-optim(m.beta, fn = cal_cost, parameters = parameters, data=covid_by_state2, grouping = 'grp1', group='W')
-grp1_MW<-optim(m.beta, fn = cal_cost, parameters = parameters, data=covid_by_state2, grouping = 'grp1', group='MW')
+write.csv(grp1_NE$par, "results/grp1_NE_betas(monthly)2.csv")
+grp1_S<-optim(m.beta, fn = cal_cost, parameters = parameters, data=covid_by_state2, grouping = 'grp1', group='S', method="BFGS")
+write.csv(grp1_S$par, "results/grp1_S_betas(monthly)2.csv")
+grp1_W<-optim(m.beta, fn = cal_cost, parameters = parameters, data=covid_by_state2, grouping = 'grp1', group='W', method="BFGS")
+write.csv(grp1_W$par, "results/grp1_W_betas(monthly)2.csv")
+grp1_MW<-optim(m.beta, fn = cal_cost, parameters = parameters, data=covid_by_state2, grouping = 'grp1', group='MW', method='BFGS')
+write.csv(grp1_MW$par, "results/grp1_MW_betas(monthly)2.csv")
 
 
 #########################
@@ -189,11 +195,16 @@ dt_grp1_o <- covid_by_state2 %>%
   group_by(grp1, date) %>%
   summarize(w_case_rate = weighted.mean(case_rate_sm, pop2020))%>%
   mutate(type='observed')
+# calibrated betas
+grp1_NE_betas <- read.csv("results/grp1_NE_betas.csv")$x
+grp1_S_betas <- read.csv("results/grp1_S_betas.csv")$x
+grp1_W_betas <- read.csv("results/grp1_W_betas.csv")$x
+grp1_MW_betas <- read.csv("results/grp1_MW_betas.csv")$x
 # modeled case rate
-case_rate_grp1_NE = run_SIRS_clbr(grp1_NE$par,parameters)[[2]]
-case_rate_grp1_S = run_SIRS_clbr(grp1_S$par,parameters)[[2]]
-case_rate_grp1_W = run_SIRS_clbr(grp1_W$par,parameters)[[2]]
-case_rate_grp1_MW = run_SIRS_clbr(grp1_MW$par,parameters)[[2]]
+case_rate_grp1_NE = run_SIRS_clbr(grp1_NE_betas,parameters)[[2]]
+case_rate_grp1_S = run_SIRS_clbr(grp1_S_betas,parameters)[[2]]
+case_rate_grp1_W = run_SIRS_clbr(grp1_W_betas,parameters)[[2]]
+case_rate_grp1_MW = run_SIRS_clbr(grp1_MW_betas,parameters)[[2]]
 dt_grp1_m <- data.frame(
   date = dt_grp1_o$date,
   MW = case_rate_grp1_MW,
@@ -214,7 +225,7 @@ ggplot(dt_grp1_all, aes(date,w_case_rate,group=type,color=type)) +
   geom_line()+
   facet_wrap(~grp1)+
   theme_bw()
-
+ggsave("graphs/calibration_fit(monthly).pdf")
 
 # Test run with IC = c(1,0.01,0)
 I.all <- data.frame(time=seq(0,Tend),green=prob.gr[,3],blue=prob.bl[,3],red=prob.rd[,3])
